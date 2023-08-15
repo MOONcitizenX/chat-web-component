c7c862e92d6b17834508aef8fea2d1f8e3597e5f
MOONcitizenX <mooncitizenx@gmail.com>
2023-08-15 19:12:05 +0300

    refactor: make default chat

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,11 @@
 <script setup lang="ts">
 import { register } from './chat-widget'
 register('chat-bot-widget')
-
-const tasks = [
-  {
-    text: '1 задание',
-    response: 'Хорошо, я сделаю 1 задание. Что еще могу сделать?',
-    action: () => {
-      console.log('1 task')
-    }
-  },
-  {
-    text: '2 задание',
-    response: 'Хорошо, я сделаю 2 задание. Что еще могу сделать?',
-    action: () => {
-      console.log('2 task')
-    }
-  },
-  {
-    text: '3 задание',
-    response: 'Хорошо, я сделаю 3 задание. Что еще могу сделать?',
-    action: () => {
-      console.log('3 task')
-    }
-  }
-]
 </script>
 
 <template>
   <main class="wrapper">
-    <chat-bot-widget
-      headerText="My chat bot"
-      botBgColor="#89C7E0"
-      botTextColor="#33373D"
-      clientBgColor="#DAE9B0"
-      clientTextColor="#12272A"
-      placeholder="My placeholder"
-      :botTasks="tasks"
-    />
+    <chat-bot-widget />
   </main>
 </template>
 
